a10855e9e00100fdf5eef7dd0b800e46f46169cb
badalkambale1155 <badalkambale16@gmail.com>
2023-11-27 16:07:06 +0530

    second commit
    adding footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {
+    FaFacebook,
+    FaTwitter,
+    FaInstagram,
+    FaAndroid,
+    FaApple,
+    FaLinkedin,
+  } from "react-icons/fa"
 
 export default function Footer() {
   return (
     <div className=''>
         
-
+{/* 
 <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 mb-0">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to={'/'} class="hover:underline">FireFistAce55</Link>. All Rights Reserved.
@@ -25,7 +33,79 @@ export default function Footer() {
         </li>
     </ul>
     </div>
-</footer>
+</footer> */}
+ <footer className="footer bg-slate-400 mt-1">
+        <a href="/"></a>
+        <div>
+          <h4>Quick links</h4>
+          <a href="/about" className="quick-links">
+            <p>About</p>
+          </a>
+          <a href="/" className="quick-links">
+            <p>Contact Us</p>
+          </a>
+          <a href="/" className="quick-links">
+            <p>Terms & Conditions</p>
+          </a>
+          <a href="/" className="quick-links">
+            <p>Home Loan</p>
+          </a>
+          <a href="/" className="quick-links">
+            <p>Privacy-Policy</p>
+          </a>
+        </div>
+        <div className="followus">
+          <h4>Follow us on</h4>
+          <a href="">
+            <FaFacebook />
+          </a>
+          <a href="">
+            <FaTwitter />
+          </a>
+          <a href="">
+            <FaInstagram />
+          </a>
+          <a href="">
+            <FaLinkedin />
+          </a>
+        </div>
+        <div>
+          <h4>Our Partners</h4>
+          <a href="/" className="partners">
+            <p>Housing.com</p>
+          </a>
+          <a href="/" className="partners">
+            <p>Prootiger.com</p>
+          </a>
+          <a href="/" className="partners">
+            <p>Realtor.com</p>
+          </a>
+          <a href="/" className="partners">
+            <p>Rent.com</p>
+          </a>
+        </div>
+        <div>
+          <h4>Buyer App|seller App</h4>
+          <a href="" className="buyerseller">
+            <FaAndroid />
+          </a>
+          <a href="" className="buyerseller">
+            <FaApple />
+          </a>
+          |
+          <a href="" className="buyerseller">
+            <FaAndroid />
+          </a>
+          <a href="" className="buyerseller">
+            <FaApple />
+          </a>
+        </div>
+        <hr></hr>
+      </footer>
+      <div className="bottomline">
+        <hr></hr>
+        <address>Copywrite&copy; 2023 FireFistAce55.com| All Rights Reserved</address>
+      </div>
 
     </div>
   )
